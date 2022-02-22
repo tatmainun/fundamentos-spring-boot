2f22dd7e76bbd9396f638d241460a392ddc905f7
tatmainun <tatiana.mailen.nunes@gmail.com>
2022-02-22 01:28:50 -0300

    Commit inicial

---
+++ src/main/java/com/fundamentosspringboot/fundamentosspringboot/FundamentosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosspringboot.fundamentosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosspringboot/fundamentosspringboot/FundamentosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosspringboot.fundamentosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
